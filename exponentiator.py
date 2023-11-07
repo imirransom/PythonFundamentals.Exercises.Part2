b3ef20f8b9cfb77d5d89b9445446c7c09ad4e6e1
Imir <imir@Zipcoders-MacBook-Pro-4.local>
2023-11-07 16:38:49 -0500

    finished

--- exponentiator.py
+++ exponentiator.py
@@ -10,4 +10,12 @@ square = lambda x: exponentiate(x, 2)
 
 cube = lambda x: exponentiate(x, 3)
 
+print(square(2))
+print(cube(2))
+print(raise_to_the_fourth_power(2))
 
+
+first = input("First ")
+second = input("Second ")
+answer = float(first) + float(second)
+print(answer)
